b027419ab987971337e2386b29e509bd121cc9e6
Dedy Wahyudi <d3dyw4hyud1@gmail.com>
2019-04-25 18:19:07 +0700

    scroll fixes

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,21 @@
 $(function() {
-  console.log("We're here");
-  $('#scroll_down').on('click', function(e) {
-    console.log("In click Event");
-    var hash = this.hash;
-    console.log("$(hash) = " + $(hash).offset);
-    e.preventDefault();
-    $('html, body').animate({ scrollTop: ($(hash).offset().top - 520)}, 700, 'linear');
+  // console.log("We're here");
+  // $('#scroll_down').on('click', function(e) {
+  //   console.log("In click Event");
+  //   var hash = this.hash;
+  //   console.log("$(hash) = " + $(hash).offset);
+  //   e.preventDefault();
+  //   $('html, body').animate({ scrollTop: ($(hash).offset().top - 520)}, 700, 'linear');
+  // });
+
+  $('#scroll_down').click(function(e) {
+  	e.preventDefault();
+
+  	var position = $($(this).attr("href")).offset().top-60;
+
+  	$("body, html").animate({
+  		scrollTop: position
+  	}, 500 );
   });
 });
 
